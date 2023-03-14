bb1d2ab6d51eed43251cf82f7eb76eed320e05bd
Abel1027 <arodriguezm1027@gmail.com>
2023-03-14 12:38:00 +0100

    [TEST] Add tests

--- lib/material_state_builder.dart
+++ lib/material_state_builder.dart
@@ -1,6 +1,5 @@
-/// A widget that offers the dimensions (width and height)
-/// and the start offset of the widget you return from a
-/// [builder] callback function.
+/// A widget that offers the material states and its corresponding controller to
+/// a material widget.
 library material_state_builder;
 
 export 'src/material_state_builder_widget.dart';
--- test/material_state_builder_test.dart
+++ test/material_state_builder_test.dart
@@ -1,1 +1,56 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:material_state_builder/material_state_builder.dart';
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialStateBuilder(
+      /// Easy way to set the same color for both the icon and the
+      /// button for every button state.
+      builder: (controller, states) {
+        /// Default colot.
+        var color = Colors.blue;
+        if (states.contains(MaterialState.pressed)) {
+          /// Change color when button is pressed.
+          color = Colors.red;
+        }
+
+        return Row(
+          children: [
+            Icon(Icons.person, color: color),
+            ElevatedButton(
+              statesController: controller,
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(color),
+              ),
+              onPressed: () {},
+              child: const Text('Press me'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+void main() {
+  testGoldens('MaterialStateBuilder', (tester) async {
+    final builder = GoldenBuilder.grid(columns: 2, widthToHeightRatio: 1)
+      ..addScenario('Default state', const MyApp())
+      ..addScenario('Pressed state', const MyApp());
+
+    await tester.pumpWidgetBuilder(
+      builder.build(),
+      surfaceSize: const Size(360, 140),
+    );
+
+    final buttonFinder = find.byType(ElevatedButton);
+    await tester.press(buttonFinder.last);
+
+    await screenMatchesGolden(tester, 'material_state_builder_golden_test');
+  });
+}
